b8609c647e3b04d95aa5a9acfc4043a39c25a325
OwenxOwen <oreynold@ashland.edu>
2025-11-05 18:09:50 -0500

    Started Header

---
+++ frontend/app/components/Header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+
+export default function Header(){
+    return(
+        <header className="flex justify-center w-full">
+            <div className="flex items-center justify-between p-5 w-4/5">
+                <div className="">
+                    <h2 className="">Owen Reynolds</h2>
+                </div>
+                <div className="flex gap-5">
+                    <Link
+                        href={"/professional"}>
+                        Professional
+                    </Link>
+                    <Link
+                        href={"/aboutme"}>
+                        About Me
+                    </Link>
+                    <Link
+                        href={"/gallery"}>
+                        Gallery
+                    </Link>
+                </div>
+            </div>
+        </header>
+    )
+}
